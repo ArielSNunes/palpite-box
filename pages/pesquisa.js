f97895cd16c34767068390b899e21b72082a8526
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-01-18 22:05:50 -0300

    Updated pesquisa layout

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -3,11 +3,32 @@ import Link from 'next/link'
 
 const Pesquisa = () => {
 	return (
-		<div>
-			<h1>Pesquisa</h1>
-			<Link href='/'>
-				Home
-			</Link>
+		<div className='pt-6'>
+			<h1 className='my-4 text-center font-bold text-2xl'>Criticas e sugestões</h1>
+			<p className='my-6 text-center'>
+				O restaurante X sempre busca por atender melhor seus clientes.
+				<br />
+				Por isso, estamos sempre abertos a ouvir a sua opinião.
+			</p>
+			<div className='flex items-center justify-start flex-col mb-6 mt-3'>
+				<div className='flex items-start flex-col w-80'>
+					<label htmlFor="">Seu nome:</label>
+					<input type="text" className='p-4 block shadow bg-blue-100 my-2 rounded-md w-[100%]' />
+				</div>
+				<div className='flex items-start flex-col w-80'>
+					<label htmlFor="">E-mail:</label>
+					<input type="text" className='p-4 block shadow bg-blue-100 my-2 rounded-md w-[100%]' />
+				</div>
+				<div className='flex items-start flex-col w-80'>
+					<label htmlFor="">WhatsApp:</label>
+					<input type="text" className='p-4 block shadow bg-blue-100 my-2 rounded-md w-[100%]' />
+				</div>
+				<div className='flex items-start flex-col w-80'>
+					<label htmlFor="">Sua critica ou sugestão:</label>
+					<input type="text" className='p-4 block shadow bg-blue-100 my-2 rounded-md w-[100%]' />
+				</div>
+				
+			</div>
 		</div>
 	)
 }
