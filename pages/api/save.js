bbcaede863aaa5a6075e5a4b4d207b2b7e7bc883
Ariel dos Santos Nunes <arielsn@outlook.com>
2022-01-23 15:48:55 -0300

    Updated state and included moment dates

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,4 +1,6 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
+import moment from 'moment'
+
 import credentials from '../../credentials.json'
 
 const sheetId = '1v9y1toMozR63PaiV01zl7F9xm8ae7su0Koi7PF9fbLU'
@@ -14,19 +16,34 @@ const doc = new GoogleSpreadsheet(sheetId)
 export default async (req, res) => {
 	const body = JSON.parse(req.body)
 
+	const formData = {
+		Nome: body.nome,
+		'E-mail': body['E-mail'],
+		Whatsapp: body.Whatsapp,
+		Cupom: '',
+		Promo: '',
+		'Data Preenchimento': moment().format('DD/MM/YYYY HH:mm:ss'),
+		Nota: 5
+	}
 	try {
 		await doc.useServiceAccountAuth(credentials)
 		await doc.loadInfo()
 
 		const sheet = doc.sheetsByIndex[1]
 
-		await sheet.addRow({
-			Nome: body.nome,
-			'E-mail': body['E-mail'],
-			Whatsapp: body.Whatsapp,
-			Cupom: new Date().getTime(),
-			Promo: new Date().getTime()
-		})
+		const configSheet = doc.sheetsByIndex[2]
+		await configSheet.loadCells('A2:B3')
+
+		const mostarPromo = (configSheet.getCell(2, 0)).value
+		const texto = (configSheet.getCell(2, 1)).value
+
+		if (mostarPromo === 'VERDADEIRO') {
+			formData.Promo = texto
+			formData.Cupom = 'teste'
+		}
+
+		console.log(formData)
+		await sheet.addRow(formData)
 
 		return res.json(body)
 	} catch (error) {
